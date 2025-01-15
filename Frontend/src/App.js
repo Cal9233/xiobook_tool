80a24b6f93f1cb24d23437167f5fa67a57b16033
Cal <calvin.m71@hotmail.com>
2025-01-15 15:11:59 -0800

    prefabs

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Login from "./pages/Login";
-import Dashboard from './pages/Dashboard'
+import Dashboard from "./pages/Dashboard";
 
 import { Box } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -9,15 +9,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     // <AppProvider>
-      <BrowserRouter>
-        {/* <Navbar /> */}
-        <Box sx={{ padding: "10px" }}>
-          <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route exact path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </Box>
-      </BrowserRouter>
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      <Box sx={{ padding: "10px" }}>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
     // </AppProvider>
   );
 }
