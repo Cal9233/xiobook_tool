8193fc24d0ce1f062bd4f25a21fdeb8b792624d1
Cal <calvin.m71@hotmail.com>
2025-01-30 10:23:54 -0800

    update

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -6,7 +6,11 @@ const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
 con.connect(function(err) {
